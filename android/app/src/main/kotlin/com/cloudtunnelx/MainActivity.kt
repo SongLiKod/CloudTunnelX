6a821f9fed6580a90b00df96bfa9b8c1746c9a3d
imuer <song.lee.vip@outlook.com>
2026-09-06 18:42:26 +0800

    #1 更新版本问题

--- android/app/src/main/kotlin/com/cloudtunnelx/MainActivity.kt
+++ android/app/src/main/kotlin/com/cloudtunnelx/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
